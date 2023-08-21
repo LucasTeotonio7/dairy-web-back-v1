e1306327a846a3da60e1ca9fd72d12a8a39ba15b
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-08-21 20:47:50 -0300

    feat: created user model

---
+++ apps/core/models/user.py
@@ -0,0 +1,62 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, username, email, name,last_name, password, is_staff, is_superuser, **extra_fields):
+        user = self.model(
+            username = username,
+            email = email,
+            name = name,
+            last_name = last_name,
+            is_staff = is_staff,
+            is_superuser = is_superuser,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, username, email, name,last_name, password=None, **extra_fields):
+        return self._create_user(username, email, name,last_name, password, False, False, **extra_fields)
+
+    def create_superuser(self, username, email, name,last_name, password=None, **extra_fields):
+        return self._create_user(username, email, name,last_name, password, True, True, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True,)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to='profile/', max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    class Meta:
+        db_table = 'user'
+        ordering = ['username']
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email','name','last_name']
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='core_users'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='core_users'
+    )
