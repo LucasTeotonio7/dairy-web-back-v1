151c1097bdb6229a42e1738bed52fbe3b03db326
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-08-22 20:20:10 -0300

    refactor: separate user manage in a new file

--- apps/core/models/user.py
+++ apps/core/models/user.py
@@ -1,29 +1,10 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
+from apps.core.models.mixins.user_manager import UserManager
 
-class UserManager(BaseUserManager):
-    def _create_user(self, username, email, name,last_name, password, is_staff, is_superuser, **extra_fields):
-        user = self.model(
-            username = username,
-            email = email,
-            name = name,
-            last_name = last_name,
-            is_staff = is_staff,
-            is_superuser = is_superuser,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self.db)
-        return user
-
-    def create_user(self, username, email, name,last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name,last_name, password, False, False, **extra_fields)
-
-    def create_superuser(self, username, email, name,last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name,last_name, password, True, True, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
