dfdab82d55eb841991dc430750f64463592e0d47
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-08-23 19:56:18 -0300

    feat: created model Brand

---
+++ apps/product/models/brand.py
@@ -0,0 +1,19 @@
+import uuid
+
+from django.db import models
+
+from apps.core.models.mixins.timestamped_model import TimestampedModel
+
+
+class Brand(TimestampedModel):
+    measure_unit_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        db_table = 'brand'
+        ordering = ['created_at']
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
+    def __str__(self):
+        return self.description
