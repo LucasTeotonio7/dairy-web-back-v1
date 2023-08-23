e99487a786dfdbf52ce8413be631cef06eca70d5
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-08-22 21:35:15 -0300

    feat: created timestamped model mixin

---
+++ apps/core/models/mixins/timestamped_model.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from django.db import models
+
+
+class SoftDeletionManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=None)
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(null=True, blank=True)
+
+    objects = SoftDeletionManager()
+    dm_objects = models.Manager()
+    
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted = datetime.now()
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
+
+    def undelete(self):
+        self.deleted = None
+        self.save()
