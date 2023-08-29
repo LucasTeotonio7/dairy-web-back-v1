779d45ad3c07a2b64602cc474548462636524e2c
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-08-29 20:33:32 -0300

    chore: enabled local debug with ide vscode

--- manage.py
+++ manage.py
@@ -2,11 +2,22 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import logging
+
+from django.conf import settings
+
+logging.basicConfig(level=logging.INFO, format='\n \033[92m %(message)s \033[0m \n')
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
+
+    # debug
+    if os.environ.get('RUN_MAIN') and settings.APP_ENV == 'local':
+        import ptvsd
+        logging.info('DEBUG SERVER STARTED AT PORT 3000')
+        ptvsd.enable_attach(address=('0.0.0.0', 3000))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
