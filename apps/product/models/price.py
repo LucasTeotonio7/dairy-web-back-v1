baf502b449a44c86756d5756fd94d7cff2961a58
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-08-30 18:37:38 -0300

    feat: added default field in price model

--- apps/product/models/price.py
+++ apps/product/models/price.py
@@ -11,6 +11,7 @@ class Price(TimestampedModel):
     price_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     value = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.CharField(max_length=50, blank=True, null=True)
+    default = models.BooleanField(default=False)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT)
 
