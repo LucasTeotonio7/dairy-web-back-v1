f6e1cc9d43074ccc12ae88688f3b9aae98a6960a
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-09-20 20:27:00 -0300

    chore: adjusted imaging endpoint in local env

--- conf/urls.py
+++ conf/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -9,3 +11,6 @@ urlpatterns = [
     path('api/', include('apps.core.api.routers')),
     path('api/', include('apps.product.api.routers')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
