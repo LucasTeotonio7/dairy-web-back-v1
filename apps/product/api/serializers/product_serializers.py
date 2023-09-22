663856c11ff8b77fd428fd7b3dde89fc7f40a628
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-09-21 21:35:06 -0300

    feat: added field unit_quantity to product serializer
    overridden to_representation method

--- apps/product/api/serializers/product_serializers.py
+++ apps/product/api/serializers/product_serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from apps.product.models import Product
 from apps.core.api.serializers.base_serializers import BaseSerializer
 
@@ -7,9 +9,19 @@ class ProductSerializer(BaseSerializer):
     class Meta:
         model = Product
         exclude = ['deleted']
-        custom_fields = ['description', 'brand', 'category', 'measure_unit', 'active', 'image']
+        custom_fields = ['description', 'brand', 'category', 'unit_quantity', 'measure_unit', 'active', 'image']
         methods = ['create', 'update', 'partial_update']
 
     def create(self, validated_data):
         validated_data['created_by'] = self.user
         return super().create(validated_data)
+    
+    def get_category(self, validated_data):
+        return validated_data
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['brand'] = instance.brand.description
+        representation['category'] = instance.category.description
+        representation['measure_unit'] = instance.measure_unit.abbreviation
+        return representation
