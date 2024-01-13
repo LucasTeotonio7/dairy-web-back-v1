274c0c75761df6cf0f7d66ac1134aeaf779aefbc
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-13 10:54:13 -0300

    refactor: standardized endpoint routes

--- apps/product/api/routers/__init__.py
+++ apps/product/api/routers/__init__.py
@@ -10,13 +10,13 @@ from apps.product.api.views.purchase_views import PurchaseView
 
 
 router = routers.DefaultRouter()
-router.register(r'measure_unit', MeasureUnitView, basename='measure_unit')
+router.register(r'measure-unit', MeasureUnitView, basename='measure-unit')
 router.register(r'category', CategoryView, basename='category')
 router.register(r'brand', BrandView, basename='brand')
 router.register(r'product', ProductView, basename='product')
 router.register(r'price', PriceView, basename='price')
-router.register(r'price_product_supplier', PriceProductSupplierView, basename='price_product_supplier')
-router.register(r'weekly_control', WeeklyControlView, basename='weekly_control')
+router.register(r'price-product-supplier', PriceProductSupplierView, basename='price-product-supplier')
+router.register(r'weekly-control', WeeklyControlView, basename='weekly-control')
 router.register(r'purchase', PurchaseView, basename='purchase')
 
 urlpatterns = [
