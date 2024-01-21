349b62d9bcb4fa2e41b05ce1d04c8efbc74e61fb
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-21 20:11:38 -0300

    feat: added pagination to product listing

--- apps/product/api/views/product_views.py
+++ apps/product/api/views/product_views.py
@@ -1,6 +1,7 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
+from apps.core.components import Paginator
 from apps.product.models import Product
 from apps.product.api.serializers.product_serializers import ProductSerializer, ProductDetailSerializer
 
@@ -9,6 +10,7 @@ from apps.product.api.serializers.product_serializers import ProductSerializer, 
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = Paginator
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
