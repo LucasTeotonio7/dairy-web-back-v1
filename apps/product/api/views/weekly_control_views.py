d6ac55f85bbade19c41c2404ca0dbeb35ec8adb8
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-02-14 20:51:50 -0300

    feat: added order_by by -created_at
    feat: added detail serializer to retrieve

--- apps/product/api/views/weekly_control_views.py
+++ apps/product/api/views/weekly_control_views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 
 from apps.core.components.paginator import Paginator
 from apps.product.models import WeeklyControl
-from apps.product.api.serializers.weekly_control_serializers import WeeklyControlSerializer
+from apps.product.api.serializers.weekly_control_serializers import WeeklyControlSerializer, WeeklyControlDetailSerializer
 
 
 @extend_schema(tags=['Weekly Control', ])
@@ -11,3 +11,13 @@ class WeeklyControlView(viewsets.ModelViewSet):
     queryset = WeeklyControl.objects.all()
     serializer_class = WeeklyControlSerializer
     pagination_class = Paginator
+
+    def get_queryset(self):
+        queryset = WeeklyControl.objects.all()
+        return queryset.order_by('-created_at')
+    
+
+    def get_serializer_class(self):
+        if self.action in ['retrieve']:
+            return WeeklyControlDetailSerializer
+        return super().get_serializer_class()
