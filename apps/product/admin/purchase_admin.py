4dedaa22220412bf5114aaee526029e79d986c5f
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-02-20 20:30:35 -0300

    feat: add field weekly_control to purchase adm

--- apps/product/admin/purchase_admin.py
+++ apps/product/admin/purchase_admin.py
@@ -4,7 +4,7 @@ from apps.product.models import Purchase
 
 
 class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'reference_day', 'quantity', 'is_closed', 'product', 'supplier', 'created_by')
+    list_display = ('id', 'reference_day', 'weekly_control', 'quantity', 'is_closed', 'product', 'supplier', 'created_by')
     list_display_links = ('id',)
     list_filter = ('is_closed',)
     search_fields = ('id', 'product__name', 'supplier__name', 'created_by__username')
