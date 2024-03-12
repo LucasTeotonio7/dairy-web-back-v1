e18d0bb1806b7cf7e0ab2b23ce334c2ba28a5dc9
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-03-11 23:09:58 -0300

    feat: added price table value

--- apps/product/services/weekly_control_services.py
+++ apps/product/services/weekly_control_services.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from apps.core.models import Supplier
-from apps.product.models import Purchase, WeeklyControl
+from apps.product.models import Purchase, WeeklyControl, Price
 
 
 def get_weekly_control_purchases_by_supplier(weekly_control: WeeklyControl, supplier_id: str = None):
@@ -25,22 +25,38 @@ def get_weekly_control_purchases_by_supplier(weekly_control: WeeklyControl, supp
         total_quantity = 0.00
         for date in date_range:
             quantity = 0.00
+            price = 0.00
             id = None
             purchase_by_reference_day = purchases_by_supplier.filter(reference_day=date).first()
             if purchase_by_reference_day:
                 quantity = purchase_by_reference_day.quantity
                 id = purchase_by_reference_day.id
+                price = purchase_by_reference_day.unit_price
             purchases.append({
                 'id': id,
                 'reference_day': date.strftime('%Y-%m-%d'),
                 'quantity':  quantity,
+                'price': price,
                 'weekday': date.weekday()
             })
             total_quantity += float(quantity)
 
+        price_table = supplier.priceproductsupplier_set.first()
+        price_value = 0.00
+        if not price_table:
+            price_default = Price.objects.filter(
+                product=weekly_control.product, 
+                default=True
+            ).first()
+            if price_default:
+                price_value = float(price_default.value)
+        else:
+            price_value = float(price_table.price.value)
+
         result.append({
             'id': supplier.id,
             'name': supplier.name,
+            'price': price_value,
             'purchases': purchases,
             'total_quantity': total_quantity
         })
