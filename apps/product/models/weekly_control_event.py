f52b7fc3c5459b3e7c1ef332976f18acd4cd0689
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-05-01 20:41:10 -0300

    feat: added new fields in weekly control events

--- apps/product/models/weekly_control_event.py
+++ apps/product/models/weekly_control_event.py
@@ -13,17 +13,21 @@ class WeeklyControlEvent(TimestampedModel):
         RECORD = 'RECORD', 'Registro'
         PAYMENT = 'PAYMENT', 'Pagamento'
         PRICE = 'PRICE', 'Preço'
+        MANUAL = 'MANUAL', 'Manual'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(max_length=7, choices=Type.choices)
     description = models.CharField(max_length=255, blank=True, null=True)
+    old_value = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    new_value = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    reference_day = models.DateField(null=True, blank=True)
     supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT)
     weekly_control = models.ForeignKey(WeeklyControl, on_delete=models.PROTECT)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'weekly_control_event'
-        ordering = ['created_at']
+        ordering = ['-created_at']
         verbose_name = 'Weekly control event'
         verbose_name_plural = 'Weekly control events'
 
