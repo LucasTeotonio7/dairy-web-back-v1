3cb8d9c6c9ca22bd07c83fc7fdba03b9438d7c9c
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-05-02 20:00:58 -0300

    feat: added user object field and measure_unit field

--- apps/product/api/serializers/weekly_control_event_serializers.py
+++ apps/product/api/serializers/weekly_control_event_serializers.py
@@ -1,8 +1,20 @@
+from rest_framework import serializers
+
+from apps.core.models import User
 from apps.product.models import WeeklyControlEvent
 from apps.core.api.serializers.base_serializers import BaseSerializer
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'name', 'last_name', 'image', 'is_active', 'is_staff')
+
+
 class WeeklyControlEventSerializer(BaseSerializer):
+
+    created_by = UserSerializer()
+    measure_unit = serializers.CharField(source='weekly_control.product.measure_unit.abbreviation')
 
     class Meta:
         model = WeeklyControlEvent
