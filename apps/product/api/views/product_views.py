c020cf91d5ea495a722b107d656a7078f82c062e
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-06-09 19:32:00 -0300

    feat: added default table filter

--- apps/product/api/views/product_views.py
+++ apps/product/api/views/product_views.py
@@ -2,7 +2,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
 from apps.core.components import Paginator
-from apps.product.models import Product
+from apps.product.models import Product, Price
 from apps.product.api.serializers.product_serializers import ProductSerializer, ProductDetailSerializer
 
 
@@ -13,12 +13,19 @@ class ProductView(viewsets.ModelViewSet):
     pagination_class = Paginator
 
     def get_queryset(self):
+        queryset = super().get_queryset()
         no_pagination = self.request.query_params.get('no_paginate', None)
+        default_table = self.request.query_params.get('default_table')
         if no_pagination:
             self.pagination_class = None
+        if default_table:
+            product_ids = Price.objects.filter(
+                product__in=queryset, default=True
+            ).values_list('product', flat=True)
+            queryset = queryset.filter(pk__in=product_ids)
         else:
             self.pagination_class = Paginator
-        return super().get_queryset()
+        return queryset
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
