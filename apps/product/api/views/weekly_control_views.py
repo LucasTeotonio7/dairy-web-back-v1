b85105bb6a2635ff83214f7a9873058087351e53
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-06-16 22:03:37 -0300

    feat: prevent editing or deletion of closed spreadsheets

--- apps/product/api/views/weekly_control_views.py
+++ apps/product/api/views/weekly_control_views.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from apps.core.components.paginator import Paginator
 from apps.product.models import WeeklyControl
@@ -21,3 +22,25 @@ class WeeklyControlView(viewsets.ModelViewSet):
         if self.action in ['retrieve']:
             return WeeklyControlDetailSerializer
         return super().get_serializer_class()
+
+    def check_if_closed(self, weekly_control):
+        if weekly_control.is_closed:
+            return Response(
+                data={'message': 'Closed spreadsheet cannot be changed.'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        return None
+
+    def update(self, request, *args, **kwargs):
+        weekly_control: WeeklyControl = self.get_object()
+        response = self.check_if_closed(weekly_control)
+        if response:
+            return response
+        return super().update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        weekly_control: WeeklyControl = self.get_object()
+        response = self.check_if_closed(weekly_control)
+        if response:
+            return response
+        return super().destroy(request, *args, **kwargs)
