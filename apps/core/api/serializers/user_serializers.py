a04007140f9147ad648af95a2515a450b30fe4db
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-07-21 20:26:55 -0300

    feat: created endpoints to set password

--- apps/core/api/serializers/user_serializers.py
+++ apps/core/api/serializers/user_serializers.py
@@ -1,3 +1,6 @@
+
+from rest_framework import serializers
+
 from apps.core.api.serializers.base_serializers import BaseSerializer
 from apps.core.models import User
 
@@ -6,6 +9,16 @@ class UserSerializer(BaseSerializer):
 
     class Meta:
         model = User
-        exclude = ['deleted', 'password']
+        exclude = ['deleted', 'password', 'groups', 'user_permissions']
         custom_fields = ['username', 'email', 'name', 'last_name', 'is_active', 'image']
         methods = ['create', 'update', 'partial_update']
+
+
+class SetPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("As senhas não coincidem.")
+        return data
--- apps/core/api/views/user_views.py
+++ apps/core/api/views/user_views.py
@@ -1,7 +1,9 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from apps.core.api.serializers.user_serializers import UserSerializer
+from apps.core.api.serializers.user_serializers import SetPasswordSerializer, UserSerializer
 from apps.core.components import Paginator
 from apps.core.models import User
 
@@ -16,3 +18,31 @@ class UserView(viewsets.ModelViewSet):
         queryset = super().get_queryset()
         queryset = queryset.filter(deleted=None)
         return queryset
+
+    def dispatch(self, request, *args, **kwargs):
+        if any(path in request.path for path in ['get-user-no-validate', 'set-password']):
+            self.authentication_classes = []
+            self.permission_classes = []
+        return super().dispatch(request, *args, **kwargs)
+
+    @action(detail=False, methods=['get'], url_path='get-user-no-validate')
+    def get_user_no_validate(self, request):
+        username = self.request.GET.get('username')
+        user = User.objects.filter(username=username, password__exact='', last_login=None).first()
+        if user:
+            user_serializer = self.get_serializer(user)
+            return Response({**user_serializer.data})
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=True, methods=['post'], url_path='set-password', serializer_class=SetPasswordSerializer)
+    def set_password(self, request, pk=None):
+        user: User = self.get_object()
+        serializer = SetPasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        password = serializer.validated_data['password']
+        user.set_password(password)
+        user.is_active = True
+        user.save()
+
+        return Response({'status': 'senha atualizada com sucesso'}, status=status.HTTP_200_OK)
