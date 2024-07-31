e8a910f6c697cc2f46d90f041ef9975615c75e7e
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-07-31 19:44:57 -0300

    feat: added groups in user serializer

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.6.7'
+__version__ = '0.6.8'
 __package__ = 'dairy_web_back_v1'
--- apps/core/api/serializers/user_serializers.py
+++ apps/core/api/serializers/user_serializers.py
@@ -1,17 +1,33 @@
 
 from rest_framework import serializers
+from django.contrib.auth.models import Group
 
 from apps.core.api.serializers.base_serializers import BaseSerializer
 from apps.core.models import User
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name'] 
+
+
 class UserSerializer(BaseSerializer):
+    available_groups = serializers.SerializerMethodField()
+    groups = serializers.PrimaryKeyRelatedField(
+        queryset=Group.objects.all(),
+        many=True,
+        required=False
+    )
 
     class Meta:
         model = User
-        exclude = ['deleted', 'password', 'groups', 'user_permissions']
-        custom_fields = ['username', 'email', 'name', 'last_name', 'is_active', 'image']
+        exclude = ['deleted', 'password', 'user_permissions']
+        custom_fields = ['username', 'email', 'name', 'last_name', 'is_active', 'image', 'groups']
         methods = ['create', 'update', 'partial_update']
+
+    def get_available_groups(self, obj):
+        return Group.objects.values('id', 'name')
 
 
 class SetPasswordSerializer(serializers.Serializer):
